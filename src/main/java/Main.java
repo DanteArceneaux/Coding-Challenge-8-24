cf2bde9febf579fed480a5cb7c73a130c07c77ff
DanteArceneaux <90316637+DanteArceneaux@users.noreply.github.com>
2022-08-24 01:48:47 +0700

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,50 @@
-import java.util.*;
-import java.lang.reflect.*;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
-
 public class Main {
     public static void main(String[] args) {
 
+        int[] nums1 = {1, 2,};
+        int[] nums2 = {3, 4};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+    }
 
- int number  = 45;
+    private static int findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        System.out.println(number.);
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
+                iMin = i + 1;
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
+                iMax = i - 1;
+            } else {
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+                int minRight = 0;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
+                }
+                return (int) ((maxLeft + minRight) / 2.0);
+            } //time complexity is O(log(min(m,n)))
+        }
+        return 0;
     }
 }
 
